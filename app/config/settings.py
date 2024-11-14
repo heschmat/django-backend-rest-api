f5f532585b2aaf21e35db9cbfa9168b0f69d5f69
Hesch <heschmat.seyedi@gmail.com>
2024-11-14 23:24:04 +0100

    Use dummy db for initial project setup.

--- app/config/settings.py
+++ app/config/settings.py
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
+
 
 
 # Password validation
