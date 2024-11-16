7cf1ec332f587b37dda45aeea287609e0ab994f8
Hesch <heschmat.seyedi@gmail.com>
2024-11-16 14:40:43 +0100

    Add admin panel 'Users' add & change modifications.

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django Admin Panel Customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _  # future proofing
 
 from core import models
 
@@ -11,6 +12,25 @@ class UserAdmin(BaseUserAdmin):
     """Define customized `UserAdmin` class for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        (_('important dates'), {'fields': ('last_login',)})
+
+    )
+
+    readonly_fields = ['last_login']
+    # Fields for add user page
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2',
+                'name',
+                'is_active', 'is_staff', 'is_superuser'
+            )
+        }),  #don't forget the `,` => TypeError: cannot unpack non-iterable NoneType object
+    )
 
 
 # Register our `User` model with the above `UserAdmin` customization.
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,3 +31,18 @@ class AdminPanelTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test"""
+        # /admin/core/user/{user_id}/change/
+        # Make sure you pass the user_id as in a list.
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page loads with success."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
